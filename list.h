72c7696d98a828ef2667d70a11b82e9fb0e89580
Amr Halim <50851126+Amr221B@users.noreply.github.com>
2019-08-01 23:41:40 +0200

    Update list.h

--- list.h
+++ list.h
@@ -1,18 +1,29 @@
 /*
  * list.h
  *
- * This library is an implementation of a dynamic list
+ * This library is an implementation of dynamic list of integers
  *
  *  	Created on: 30/7/2019
  *      Author: Amr Halim
  */
 #ifndef LIST_H
 #define LIST_H
-void LIST_insert(int val);
-void LIST_insertAtIndex(int val, int index);
-void LIST_remove(int index);
-int LIST_get(int index);
-void LIST_edit(int val, int index);
-int LIST_count(void);
+typedef struct{
+ //pointer to list array
+ int *list;
+ //total list size
+ int size;
+ //index of the last element in the list
+ int end;
+}listType;
+
+void LIST_init(listType *ls, int size);
+int LIST_insert(listType *ls, int val);
+int LIST_insertAtIndex(listType *ls, int val, int index);
+int LIST_remove(listType *ls, int index);
+int LIST_get(listType *ls, int index);
+int LIST_edit(listType *ls, int val, int index);
+void LIST_print(listType *ls);
+int LIST_count(listType *ls);
 #endif // LIST_H
 
